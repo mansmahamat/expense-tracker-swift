1e0ced077d35a801ae069241f8318eb3b8be6d2c
mansmahamat <mansour.mahamat.salle@gmail.com>
2024-04-09 22:25:44 +0200

    Initial Commit

---
+++ Expense-tracker/Expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_trackerApp.swift
+//  Expense-tracker
+//
+//  Created by Mansour Mahamat-salle on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
